56160fe9711c7459038d875cb6b421845b07961b
Babacoders <31666887+babacoders@users.noreply.github.com>
2017-11-26 09:09:48 +0530

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,2 +1,10 @@
+
+<html><head><title><?php echo "cool title";?></title></head>
+<body>
 <?php
-phpinfo();
+$image_url='http://www.reactiongifs.com/r/cheering_minions.gif';
+?>
+
+<img src="<?php echo $image_url;?>">
+</body>
+</html>
